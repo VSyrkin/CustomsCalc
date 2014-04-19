b6fff6cc1f0edaabbedd640e3d334b914f12e8b2
VSyrkin <vs.syrkin@gmail.com>
2014-04-19 19:11:45 +0900

    Initial Commit

---
+++ CustomsCalc/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CustomsCalc
+//
+//  Created by Vladimir Syrkin on 19.04.14.
+//  Copyright (c) 2014 Vladimir Syrkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CustomsCalc/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CustomsCalc
+//
+//  Created by Vladimir Syrkin on 19.04.14.
+//  Copyright (c) 2014 Vladimir Syrkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
